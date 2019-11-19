130439f9c1574b83ed8e44838462b3bc61c7cb83
明哥 <ding43930053@126.com>
2019-11-19 08:37:41 +0800

    注释掉报错的 import 代码

--- clouddo-zuul/src/test/java/com/bootdo/clouddozuul/ClouddoZuulApplicationTests.java
+++ clouddo-zuul/src/test/java/com/bootdo/clouddozuul/ClouddoZuulApplicationTests.java
@@ -1,6 +1,4 @@
 package com.bootdo.clouddozuul;
-
-import com.bootdo.clouddozuul.prc.admin.MenuService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
