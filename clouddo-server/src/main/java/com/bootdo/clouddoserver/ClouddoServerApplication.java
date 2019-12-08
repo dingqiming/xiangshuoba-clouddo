0c2a19676967dae38f3aa62ddf80f21bbeaaae37
明哥 <ding43930053@126.com>
2019-12-08 21:08:00 +0800

    添加 身份认证

--- clouddo-server/src/main/java/com/bootdo/clouddoserver/ClouddoServerApplication.java
+++ clouddo-server/src/main/java/com/bootdo/clouddoserver/ClouddoServerApplication.java
@@ -1,11 +1,14 @@
 package com.bootdo.clouddoserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class ClouddoServerApplication {
 
 	public static void main(String[] args) {
